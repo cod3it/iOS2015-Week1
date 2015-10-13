2efdf26ce23429f4b6da82c95f5bbdbda5477a14
Alex John <alpr8791@colorado.edu>
2015-10-13 14:42:42 -0600

    100% Complete & Accurate Week 1

--- deliverables/homework.playground/Contents.swift
+++ deliverables/homework.playground/Contents.swift
@@ -1,64 +1,131 @@
-//: Shopping List - Week 1
- 
 import Foundation
 
-//: Create an empty array to store shopping list items
- var shopListArray: [String] = []
-//: Add at least 5 items to the array using 3 different insertion methods
-shopListArray.append("eggs")
-shopListArray = shopListArray + ["milk"]
-shopListArray += ["yerba", "rice", "chicken"]
-//: Sort your array of items
-shopListArray.sort() 
-//: Create a function to print an array, with it's index, with one item per line
-for(index,listItem) in shopListArray.enumerate() {
-    print("\(listItem) - \(index)")
- }
-//: Combine the above. Create a function that takes an array, sorts it and prints it with it's index, with one item per line
-func takesArray(var shopListArray: [String]) {
+//Walking through homework assignments step-by-step
+
+
+// Question #1) 
+//=============
+// Create an empty array to store shopping list items
+
+var shoppingList: [String] = []
+
+
+        // Other examples that work
+
+        var shoppingList2 = [String]()
+        var shoppingList3 = Array<String>()
+
+
+
+// Question #2)
+//=============
+// Add at least 5 items to the array using 3 different insertion methods
+
+shoppingList = ["apples"]
+shoppingList.append("bananas")
+shoppingList.insert("bread", atIndex: 2)
+
+        // Other examples that work for adding items to an array
+
+shoppingList += ["milk"]
+shoppingList.appendContentsOf(["candy"])
+
+shoppingList2 = ["eggs", "butter"]
+shoppingList.insertContentsOf(shoppingList2, at: 0)
+
+print(shoppingList)
+
+
+
+
+// Question #3)
+//=============
+// Sort your array of items
+
+shoppingList.sort()
+
+
+
+// Question #4)
+//=============
+// Create a function to print an array, with it's index, with one item per line
+
+
+func printArray(array: [String]) {
     
-    var sortedShopList = shopListArray.sort()
-    
-    for(index,listItem) in sortedShopList.enumerate() {
-        print("\(listItem) - \(index)")
+    var count: Int = 0
+    for item in array {
+        print ("\(count) - \(item)")
+        count = count + 1
     }
- }
- 
- takesArray(shopListArray)
- 
+}
 
-//: Create a function that returns a new array containing the first letter of each item, in uppercase. The returned array should be sorted and not have any repeated characters.
- func returnArray(var shopListArray: [String]) {
-    
-    var sortedArray = shopListArray.sort()
+printArray(shoppingList)
 
-    var capitalLetterArray = [String]()
+
+
+// Question #5)
+//=============
+// Combine the above. Create a function that takes an array, sorts it and prints it with it's index, with one item per line
+
+
+func printSortedArray(array: [String]) {
     
-    for index in 0 .. < sortedArray.count {
-         
-        let string = sortedArray[index]
-        var letter = String(string[string.startIndex])
-        
-        if !letters.contains(letter) {
-            letters.append(letter)
+    let sortedShoppingList = array.sort()
+    printArray(sortedShoppingList)
+    
+}
+
+// The answer uses the function created in question #4 -  printArray()  - to find the index array, simplying executing the sort command within the new function created for question #5.
+
+printSortedArray(shoppingList)
+
+
+
+// Question #6)
+//=============
+// Create a function that returns a new array containing the first letter of each item, in uppercase. The returned array should be sorted and not have any repeated characters
+
+
+func firstLetters(array: [String]) -> [String] {
+    
+    let sorted = array.sort()
+    var letters = [String]()
+    for item in sorted {
+        if let letter = item.characters.first {
+            let uppercase = String(letter).uppercaseString
+            if !letters.contains(uppercase) {
+                letters.append(uppercase)
+            }
         }
-    
-    return
-    
- }
-
-returnArray(shopListArray)
-    
-//: Create a function that searches the items array and returns all items that match the input string. Ignore uppercase/lowercase differences.
- func searchArray(var shopListArray: [String]) {
-    
-    var inputString =
-    var returnArray =
-    
-    for listItem in shopListArray {
-        
     }
     
-    return 
+    return letters
+}
+
+print(firstLetters(shoppingList))
+
+
+
+// Question #7)
+//=============
+// Create a function that searches the items array and returns all items that match the input string. Ignore uppercase/lowercase differences
+
+
+func search(array:[String], match: String) -> [String] {
+    var itemsToReturn = [String]()
     
- }
+    for item in array {
+        let lowercase = item.lowercaseString
+        if lowercase.containsString(match.lowercaseString) {
+            itemsToReturn.append(item)
+        }
+    }
+    
+    return itemsToReturn
+    
+}
+
+print(search(shoppingList, match: "y"))
+
+
